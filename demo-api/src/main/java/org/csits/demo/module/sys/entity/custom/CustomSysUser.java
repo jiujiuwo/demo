3551fe9fcaa96308261a381194f696ef67cc9432
lhf <lihaifeng94@qq.com>
2023-03-25 20:41:41 +0800

    自定义的User类，要实现CredentialsContainer 接口，登陆成功后自动消除密码。

--- demo-api/src/main/java/org/csits/demo/module/sys/entity/custom/CustomSysUser.java
+++ demo-api/src/main/java/org/csits/demo/module/sys/entity/custom/CustomSysUser.java
@@ -1,13 +1,16 @@
 package org.csits.demo.module.sys.entity.custom;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 import org.csits.demo.module.sys.entity.SysUser;
+import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.util.Collection;
 
-public class CustomSysUser extends SysUser implements UserDetails, Serializable {
+public class CustomSysUser extends SysUser implements UserDetails, CredentialsContainer, Serializable {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -31,5 +34,10 @@ public class CustomSysUser extends SysUser implements UserDetails, Serializable 
     @Override
     public boolean isEnabled() {
         return super.getDelFlag() == 0;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        super.setPassword(null);
     }
 }
