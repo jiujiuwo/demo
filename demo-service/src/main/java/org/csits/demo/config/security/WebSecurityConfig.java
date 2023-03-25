76d794b93b8d644f349e287d7a27e2f5ea1da92a
lhf <lihaifeng94@qq.com>
2023-03-25 20:41:33 +0800

    自定义的User类，要实现CredentialsContainer 接口，登陆成功后自动消除密码。

--- demo-service/src/main/java/org/csits/demo/config/security/WebSecurityConfig.java
+++ demo-service/src/main/java/org/csits/demo/config/security/WebSecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
--- demo-service/src/main/java/org/csits/demo/module/sys/controller/SysUserController.java
+++ demo-service/src/main/java/org/csits/demo/module/sys/controller/SysUserController.java
@@ -1,16 +1,20 @@
 package org.csits.demo.module.sys.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.csits.demo.api.qo.LoginQo;
 import org.csits.demo.comm.Result;
 import org.csits.demo.module.sys.entity.SysUser;
 import org.csits.demo.module.sys.entity.custom.CustomSysUser;
 import org.csits.demo.module.sys.service.ISysUserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -21,21 +25,32 @@ import org.springframework.stereotype.Controller;
  * @author lhf
  * @since 2023-03-25
  */
-@Controller
+@Tag(name = "定义用户相关的操作")
+@RestController
 @RequestMapping("/user")
 public class SysUserController {
 
     @Autowired
     private ISysUserService sysUserService;
 
+    @Operation(method = "POST", summary = "登陆验证")
     @PostMapping("/login/check")
     public Result loginCheck(@RequestBody @Validated LoginQo loginQo, @AuthenticationPrincipal CustomSysUser sysUser) {
         return Result.OK(sysUser);
     }
 
+    @Operation(method = "POST", summary = "添加用户信息")
     @PostMapping("/add")
-    public Result add(@RequestBody @Validated LoginQo loginQo) {
-        sysUserService.save(new SysUser());
-        return Result.OK();
+    public Result add(@RequestBody @Validated CustomSysUser sysUser) {
+        SysUser sysUser1 = new SysUser();
+        BeanUtils.copyProperties(sysUser, sysUser1);
+        boolean result = sysUserService.save(sysUser1);
+        return Result.OK(result);
+    }
+
+    @Operation(method = "GET", summary = "获取当前登陆的用户信息")
+    @GetMapping("/currentUser")
+    public Result currentUser(@AuthenticationPrincipal CustomSysUser sysUser) {
+        return Result.OK(sysUser);
     }
 }
